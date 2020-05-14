0a4a39afb8c57cbe756928c8e2e63211d2d7127c
Rob Easthope <hello@robeasthope.com>
2020-05-14 11:33:52 +0100

    Switch to function

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,20 @@ import Head from 'next/head';
 import { Box } from 'rebass';
 import { Basic, Combined, Animated, bounce } from '../styles/globalStyles';
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Medical Synopses</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <Box>
-      <h2>Sanity test</h2>
-      <p>Foo</p>
-    </Box>
-  </>
-);
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Medical Synopses</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Box>
+        <h2>Sanity test</h2>
+        <p>Foo</p>
+      </Box>
+    </>
+  );
+}
 
 export default Home;
