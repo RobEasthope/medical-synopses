bfa100f173ce9decb28792acea095f1d2e5334cb
Rob Easthope <hello@robeasthope.com>
2020-05-14 16:22:29 +0100

    Clean up prototype

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import groq from 'groq';
-
-import { Box } from 'rebass';
+import { Box, Heading } from 'rebass';
 
 import { getSingleSynopsis } from '../lib/api';
 
@@ -13,28 +11,16 @@ function Home({ synopsis }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Box>
-        <h2>Sanity test</h2>
-        <p>Foo</p>
-        {synopsis && <p>Title: {synopsis.title}</p>}
-      </Box>
+      <Box>{synopsis && <Heading>{synopsis.title}</Heading>}</Box>
     </>
   );
 }
 
-// This function gets called at build time on server-side.
-// It won't be called on client-side, so you can even do
-// direct database queries. See the "Technical details" section.
 export async function getStaticProps({
   params = { slug: 'foo' },
   preview = false,
 }) {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-
   const data = await getSingleSynopsis(params.slug, preview);
-
-  console.log(data);
 
   return {
     props: {
