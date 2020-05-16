31caefc4976e0b6c37c1cb178762dff49ad85917
Rob Easthope <hello@robeasthope.com>
2020-05-16 14:33:27 +0100

    Initial work new page

---
+++ pages/synopses/[synopsesSlug].js
@@ -0,0 +1,39 @@
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import Head from 'next/head';
+import { Box, Heading } from 'rebass';
+
+import { getSingleSynopsis } from '../../lib/api';
+
+function Synopsis({ synopsis }) {
+  const router = useRouter();
+  if (!router.isFallback && !synopsis?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <>
+      <Head>
+        <title>{synopsis.title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Box>{synopsis && <Heading>{synopsis.title}</Heading>}</Box>
+    </>
+  );
+}
+
+export async function getStaticProps({
+  params = { slug: 'foo' },
+  preview = false,
+}) {
+  const data = await getSingleSynopsis(params.slug, preview);
+
+  return {
+    props: {
+      preview,
+      synopsis: data || null,
+    },
+  };
+}
+
+export default Synopsis;
