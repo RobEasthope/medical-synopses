7085cb072703935cb00e85f5f15184b15b2ef17f
Rob Easthope <hello@robeasthope.com>
2020-05-17 19:35:49 +0100

    Semi-successful conversion of example code

--- pages/synopses/[slug].js
+++ pages/synopses/[slug].js
@@ -1,39 +1,48 @@
 import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
-import Head from 'next/head';
-import { Box, Heading } from 'rebass';
+import { getSynopsesWithSlug, getSynopsisAndSynopses } from '../../lib/api';
 
-import { getSingleSynopsis } from '../../lib/api';
-
-function Synopsis({ synopsis }) {
+export default function Synopsis({ synopsis, synopses, preview }) {
   const router = useRouter();
-  if (!router.isFallback && !synopsis?.slug) {
-    return <ErrorPage statusCode={404} />;
-  }
+  // if (!router.isFallback && !synopsis?.slug) {
+  //   return <ErrorPage statusCode={404} />;
+  // }
   return (
-    <>
-      <Head>
-        <title>{synopsis.title.en}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Box>{synopsis && <Heading>{synopsis.title.en}</Heading>}</Box>
-    </>
+    <div>
+      {/* // <Layout preview={preview}> */}
+      {router.isFallback ? (
+        <h1>Loading…</h1>
+      ) : (
+        <div>
+          <article>
+            <h1>{synopsis && synopsis.title.en}</h1>
+          </article>
+          {/* {synopses.length > 0 && <MoreStories synopses={synopses} />} */}
+        </div>
+      )}
+      {/* // </Layout> */}
+    </div>
   );
 }
 
-export async function getStaticProps({
-  params = { slug: 'foo' },
-  preview = false,
-}) {
-  const data = await getSingleSynopsis(params.slug, preview);
-
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getSynopsisAndSynopses(params.slug, preview);
   return {
     props: {
       preview,
-      synopsis: data || null,
+      synopsis: data?.synopsis || null,
+      synopses: data?.synopses || null,
     },
   };
 }
 
-export default Synopsis;
+export async function getStaticPaths() {
+  const synopses = await getSynopsesWithSlug();
+
+  console.log(synopses);
+
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
