27200dfde8a966030e2faa2c11fa1c00a18bff78
Rob Easthope <hello@robeasthope.com>
2020-06-27 22:44:44 +0100

    Switch to named export

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
+export class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
