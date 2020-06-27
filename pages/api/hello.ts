3bef8eac17a547a7063250adc198403e7e60df6c
Rob Easthope <hello@robeasthope.com>
2020-06-27 22:48:27 +0100

    Add function name

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export const Hello = (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
   res.json({ name: "John Doe" });
 };
