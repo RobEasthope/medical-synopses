2a7fb6a716c4faf1d685247a1f104f7490e60580
Rob Easthope <hello@robeasthope.com>
2020-06-29 12:42:28 +0100

    Disable more eslint riles for time being

--- sanity/api-calls.ts
+++ sanity/api-calls.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import sanityClient from "../settings/sanity-client";
 
 interface Synopsis {
@@ -5,10 +7,13 @@ interface Synopsis {
 }
 
 export const getSingleSynopsis = async (): Promise<Synopsis> => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const data: Synopsis = await sanityClient.fetch(`*[_type == 'synopsis'][0]{
       title,
     }`);
 
   return data;
 };
+
+/* eslint-enable @typescript-eslint/no-unsafe-member-access */
+/* eslint-enable @typescript-eslint/no-unsafe-assignment */
