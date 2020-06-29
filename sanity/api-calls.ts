d741265f586a13d47b0683b8786561acac2faaac
Rob Easthope <hello@robeasthope.com>
2020-06-29 12:49:45 +0100

    Rename function

--- sanity/api-calls.ts
+++ sanity/api-calls.ts
@@ -6,7 +6,7 @@ interface Synopsis {
   title?: string;
 }
 
-export const getSingleSynopsis = async (): Promise<Synopsis> => {
+export const getTestSynopsis = async (): Promise<Synopsis> => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   const data: Synopsis = await sanityClient.fetch(`*[_type == 'synopsis'][0]{
       title,
